6b47ef7eec6a5072db284439cfe9c8848d78c7f2
krateng <git.noreply@krateng.ch>
2025-02-17 17:19:06 +0100

    Implement ranking

--- src/database/repository/stats.rs
+++ src/database/repository/stats.rs
@@ -1,4 +1,5 @@
 use sea_orm::{ColumnTrait, DbErr, EntityTrait, JoinType, QueryFilter, QueryOrder, QuerySelect, RelationTrait};
+use sea_query::Expr;
 use crate::database::connect;
 use crate::database::repository::{resolve_artist_ids, resolve_track_ids, resolve_album_ids};
 use crate::database::views::ChartsEntry;
@@ -20,23 +21,27 @@ use crate::timeranges::TimeRange;
 pub async fn charts_tracks(timerange: TimeRange) -> Result<Vec<ChartsEntry<TrackRead>>, DbErr> {
     let db = connect().await?;
     let (from_ts, to_ts) = timerange.timestamp_boundaries();
-    let result: Vec<(u32, u32)> = Track::find()
+    let result: Vec<(u32, u32, u32)> = Track::find()
         .select_only()
         .join(JoinType::LeftJoin, entity::track::Relation::Scrobble.def())
         .column_as(TrackColumn::Id, "track_id")
         .column_as(ScrobbleColumn::Timestamp.count(), "scrobbles")
+        .column_as(
+            Expr::cust("RANK() OVER (ORDER BY COUNT(scrobbles.timestamp) DESC)"),
+            "rank"
+        )
         .filter(ScrobbleColumn::Timestamp.between(from_ts, to_ts))
         .group_by(TrackColumn::Id)
         .order_by_desc(ScrobbleColumn::Timestamp.count())
         .into_tuple()
         .all(&db).await?;
 
-    let id_list = result.iter().map(|(id, scrobbles)| id.to_owned()).collect();
+    let id_list = result.iter().map(|(id, scrobbles, rank)| id.to_owned()).collect();
     let id_map = resolve_track_ids(id_list, &db).await;
 
-    let charts: Vec<ChartsEntry<TrackRead>> = result.into_iter().map(|(id, scrobbles)| {
+    let charts: Vec<ChartsEntry<TrackRead>> = result.into_iter().map(|(id, scrobbles, rank)| {
         ChartsEntry {
-            rank: 1,
+            rank: rank,
             scrobbles: scrobbles,
             entry: id_map[&id].clone()
         }
@@ -48,25 +53,29 @@ pub async fn charts_tracks(timerange: TimeRange) -> Result<Vec<ChartsEntry<Track
 pub async fn charts_artists(timerange: TimeRange) -> Result<Vec<ChartsEntry<ArtistRead>>, DbErr> {
     let db = connect().await?;
     let (from_ts, to_ts) = timerange.timestamp_boundaries();
-    let result: Vec<(u32, u32)> = Artist::find()
+    let result: Vec<(u32, u32, u32)> = Artist::find()
         .select_only()
         .join(JoinType::LeftJoin, entity::artist::Relation::TrackArtist.def())
         .join(JoinType::LeftJoin, entity::track_artist::Relation::Track.def())
         .join(JoinType::LeftJoin, entity::track::Relation::Scrobble.def())
         .column_as(ArtistColumn::Id, "artist_id")
         .column_as(ScrobbleColumn::Timestamp.count(), "scrobbles")
+        .column_as(
+            Expr::cust("RANK() OVER (ORDER BY COUNT(scrobbles.timestamp) DESC)"),
+            "rank"
+        )
         .filter(ScrobbleColumn::Timestamp.between(from_ts, to_ts))
         .group_by(ArtistColumn::Id)
         .order_by_desc(ScrobbleColumn::Timestamp.count())
         .into_tuple()
         .all(&db).await?;
 
-    let id_list = result.iter().map(|(id, scrobbles)| id.to_owned()).collect();
+    let id_list = result.iter().map(|(id, scrobbles, rank)| id.to_owned()).collect();
     let id_map = resolve_artist_ids(id_list, &db).await;
 
-    let charts: Vec<ChartsEntry<ArtistRead>> = result.into_iter().map(|(id, scrobbles)| {
+    let charts: Vec<ChartsEntry<ArtistRead>> = result.into_iter().map(|(id, scrobbles, rank)| {
         ChartsEntry {
-            rank: 1,
+            rank: rank,
             scrobbles: scrobbles,
             entry: id_map[&id].clone()
         }
@@ -78,24 +87,28 @@ pub async fn charts_artists(timerange: TimeRange) -> Result<Vec<ChartsEntry<Arti
 pub async fn charts_albums(timerange: TimeRange) -> Result<Vec<ChartsEntry<AlbumRead>>, DbErr> {
     let db = connect().await?;
     let (from_ts, to_ts) = timerange.timestamp_boundaries();
-    let result: Vec<(u32, u32)> = Album::find()
+    let result: Vec<(u32, u32, u32)> = Album::find()
         .select_only()
         .join(JoinType::LeftJoin, entity::album::Relation::Track.def())
         .join(JoinType::LeftJoin, entity::track::Relation::Scrobble.def())
         .column_as(AlbumColumn::Id, "album_id")
         .column_as(ScrobbleColumn::Timestamp.count(), "scrobbles")
+        .column_as(
+            Expr::cust("RANK() OVER (ORDER BY COUNT(scrobbles.timestamp) DESC)"),
+            "rank"
+        )
         .filter(ScrobbleColumn::Timestamp.between(from_ts, to_ts))
         .group_by(AlbumColumn::Id)
         .order_by_desc(ScrobbleColumn::Timestamp.count())
         .into_tuple()
         .all(&db).await?;
 
-    let id_list = result.iter().map(|(id, scrobbles)| id.to_owned()).collect();
+    let id_list = result.iter().map(|(id, scrobbles, rank)| id.to_owned()).collect();
     let id_map = resolve_album_ids(id_list, &db).await;
 
-    let charts: Vec<ChartsEntry<AlbumRead>> = result.into_iter().map(|(id, scrobbles)| {
+    let charts: Vec<ChartsEntry<AlbumRead>> = result.into_iter().map(|(id, scrobbles, rank)| {
         ChartsEntry {
-            rank: 1,
+            rank: rank,
             scrobbles: scrobbles,
             entry: id_map[&id].clone()
         }
